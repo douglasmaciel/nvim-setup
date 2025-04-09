4d6d244622c3042ae05e7eadf19f13ea31cf1758
Douglas S. Maciel <contato@douglasmaciel.com>
2025-04-08 23:34:51 -0300

    return to accept completion

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -86,7 +86,8 @@ return {
             mapping = cmp.mapping.preset.insert({
                 ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
                 ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-                ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+                ['<Return>'] = cmp.mapping.confirm({ select = true }),
+                ['<C-y'] = cmp.mapping.confirm({ select = true }),
                 ["<C-Space>"] = cmp.mapping.complete(),
             }),
             sources = cmp.config.sources({
