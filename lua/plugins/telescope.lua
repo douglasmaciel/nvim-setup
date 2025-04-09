317d38d29c2395eda87c8b2ace6351315e40da6f
Douglas S. Maciel <contato@douglasmaciel.com>
2025-04-08 22:05:18 -0300

    update config as ThePrimeagen

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,20 +1,30 @@
 return {
-	"nvim-telescope/telescope.nvim",
-	tag = "0.1.6",
-	dependencies = { "nvim-lua/plenary.nvim" },
-	config = function()
-		require("telescope").setup()
-		require("telescope").load_extension("file_browser")
+    "nvim-telescope/telescope.nvim",
 
-		-- set keymaps
-		local keymap = vim.keymap
+    tag = "0.1.5",
 
-		keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
-		keymap.set("n", "<leader>bf", "<cmd>Telescope file_browser<cr>", { desc = "File browser extension" })
-		keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", { desc = "Fuzzy find recent files" })
-		keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>", { desc = "Find string in cwd" })
-		keymap.set("n", "<leader>fs", "<cmd>Telescope git_status<cr>", { desc = "Find string under cursor in cwd" })
-		keymap.set("n", "<leader>fc", "<cmd>Telescope git_commits<cr>", { desc = "Find todos" })
-		keymap.set("n", "<leader>gf", "<cmd>Telescope git_files<cr>", { desc = "Fuzzy find files in git repo" })
-	end,
+    dependencies = {
+        "nvim-lua/plenary.nvim"
+    },
+
+    config = function()
+        require('telescope').setup({})
+
+        local builtin = require('telescope.builtin')
+        vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+        vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+        vim.keymap.set('n', '<leader>pws', function()
+            local word = vim.fn.expand("<cword>")
+            builtin.grep_string({ search = word })
+        end)
+        vim.keymap.set('n', '<leader>pWs', function()
+            local word = vim.fn.expand("<cWORD>")
+            builtin.grep_string({ search = word })
+        end)
+        vim.keymap.set('n', '<leader>ps', function()
+            builtin.grep_string({ search = vim.fn.input("Grep > ") })
+        end)
+        vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
+    end
 }
+
